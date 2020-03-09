166b538703a30fe327068ba4832e3674853bdd16
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-09 04:55:57 -0300

    [AULA 16] - Iniciando aplicação

--- todos.js
+++ todos.js
@@ -1,1 +1,9 @@
-alert("Funcionou!");
+var listElements = document.querySelector("#app ul");
+var inputElements = document.querySelector("#app input");
+var buttonElements = document.querySelector("#app button");
+
+var todos = [
+    "Fazer café",
+    "Estudar Javascript",
+    "Acessar comunidade da Rocketseat"
+];
