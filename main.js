258957da92ee477fb89791d0791aff4a8751f922
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-09 05:32:51 -0300

    [AULA 22] - Promises

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 // https://api.github.com/users/zoyo23
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/users/zoyo23');
-xhr.send(null);
+var minhaPromise = function () {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/zoyo23');
+        xhr.send(null);
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+                else {
+                    reject("Erro na requisição");
+                }
+            }
+        }
+    });
 }
+
+minhaPromise()
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.warn(err);
+    });
