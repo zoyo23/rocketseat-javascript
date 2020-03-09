264326c4935bc4078e8abc5cb3ca99b2111f09e5
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-09 05:36:59 -0300

    [AULA 23] - Utilizando  AXIOS

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
 // https://api.github.com/users/zoyo23
-var minhaPromise = function () {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/zoyo23');
-        xhr.send(null);
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                }
-                else {
-                    reject("Erro na requisição");
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
+axios.get("https://api.github.com/users/zoyo23")
     .then(function (response) {
-        console.log(response);
+        console.log(response.data);
     })
     .catch(function (err) {
         console.warn(err);
