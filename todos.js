574e2191181441a4c89d609e25b67d3c6e61a2d0
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-09 05:10:32 -0300

    [AULA 19] - Excluindo TODOS

--- todos.js
+++ todos.js
@@ -14,7 +14,17 @@ function renderTodos() {
         var todoElement = document.createElement("li");
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute("onclick", "deleteTodo('" + pos + "')");
+        var linkText = document.createTextNode("Excluir");
+
+        linkElement.appendChild(linkText);
+
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElements.appendChild(todoElement);
     }
 }
@@ -30,3 +40,8 @@ function addTodo() {
 }
 
 buttonElements.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
